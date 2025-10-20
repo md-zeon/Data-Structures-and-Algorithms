1275c6ad7ec8c1536cd0ba8dcb4a1cc2e4272379
md-zeon <zeon.cse@gmail.com>
2025-10-20 14:06:12 +0600

    Learned about print, println, \n and pattern

---
+++ 03. Variables & Data Types/JavaBasics.java
@@ -0,0 +1,21 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        // This is a single-line comment
+        // Boilerplate Java program
+        System.out.print("This is a basic Java program.");
+        // It prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+        // It prints "Hello, World!" to the console
+        System.out.println("This is a basic Java program.");
+        // It prints "This is a basic Java program." to the console
+
+
+
+        // The main method is the entry point of the program
+        // System.out.print is used to print text to the console without a newline at the end
+        // System.out.println is used to print text to the console with a newline at the end
+        // ; is used to terminate statements in Java
+
+        System.out.println("Hello,\nWorld!");
+    }
+}
