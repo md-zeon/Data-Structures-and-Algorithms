8f31fac7881c2b43aa265ff226cb94401bb110ce
md-zeon <zeon.cse@gmail.com>
2025-10-22 11:54:47 +0600

    Practice Problem 1 [Solved]

---
+++ 08. Functions & Methods/Practice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Practice {
+    public static void main(String[] args) {
+        // Problem 1: Write a method to compute the average of 3 numbers.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter third number: ");
+        double num3 = scanner.nextDouble();
+        double average = computeAverage(num1, num2, num3);
+        System.out.println("The average is: " + average);
+        
+        scanner.close();
+    }
+
+    public static double computeAverage(double a, double b, double c) {
+        return (a + b + c) / 3;
+    }
+}
