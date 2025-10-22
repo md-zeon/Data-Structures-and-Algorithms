7a68e0d75e0aede4a4ac2288e77496bd098d4a12
md-zeon <zeon.cse@gmail.com>
2025-10-22 09:29:31 +0600

    Find the factorial of a given number [Solved]

--- 06. Loops/Practice.java
+++ 06. Loops/Practice.java
@@ -30,6 +30,16 @@ public class Practice {
         System.out.println("Sum of odd integers: " + sumOdd);
 
 
+        // Write a program to find the factorial of a given number using a loop.
+        System.out.println("Enter a number to find its factorial:");
+        int num = scanner.nextInt();
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + num + " is: " + factorial);
+
+
         scanner.close();
     }
 }
